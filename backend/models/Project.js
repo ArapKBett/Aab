98d35f3fcf0010a0a1de73b6780fc20af65fa0b9
Arap Bett <kipkorirjob@kabarak.ac.ke>
2025-06-07 02:48:34 -0400

    Update Project.js

--- backend/models/Project.js
+++ backend/models/Project.js
@@ -4,6 +4,7 @@ const projectSchema = new mongoose.Schema({
   name: { type: String, required: true },
   tech: { type: String, required: true },
   desc: { type: String, required: true },
+  url: { type: String }, // Added optional URL field
   createdAt: { type: Date, default: Date.now },
 });
 
